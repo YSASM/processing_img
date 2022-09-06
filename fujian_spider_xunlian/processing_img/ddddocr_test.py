2ac9202fcc5ac377f48f697d549390ac3020fd0f
YSASM <80308986+YSASM@users.noreply.github.com>
2022-09-06 18:09:11 +0800

    Delete fujian_spider_xunlian directory

--- fujian_spider_xunlian/processing_img/ddddocr_test.py
+++
@@ -1,9 +0,0 @@
-import ddddocr
-
-def recognize():
-    ocr = ddddocr.DdddOcr()
-    with open('image_store/3_img_1662457761.png', 'rb') as f:
-        img_bytes = f.read()
-    res = ocr.classification(img_bytes)
-    print(res)
-recognize()
